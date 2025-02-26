c2e507842d9c058229895f4c10f5405348b9fe37
spiknoty <vsn377507@gmail.com>
2025-02-26 03:47:39 +0000

    cadastrei as cartas do desafio super trunfo iniciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,211 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    /* irei destacar neste comentário os nomes e valores correspondentes à carta 1:
+estado: A 
+codigo: A01 
+nome da cidade: Vitoria 
+pupulação: 342.800 
+area: 97.123 km2
+pib: 63,7 BI 
+numero de pontos turisticos: 25 */
+
+// variaveis das carta 1:
+char estado1; // estado especificado apenas por uma letra, não é necessario array
+char codigocarta1[20]; 
+char nomecidade1[20];
+int populacao1;
+float areacidade1;
+float pibcidade1;
+int pontosturisticos1;
+
+// entrada de dados da carta 1:
+
+printf("digite o indicador do estado da carta 1 ('A' a 'H'): \n");
+scanf(" %c", &estado1); // espaço utilizado antes de '%c' para evitar que sejam deixados espaços em branco no buffer de entrada.
+
+printf("digite o codigo da carta 1: \n");
+scanf("%s", codigocarta1); // por 'codigocarta1' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite o nome da cidade 1: \n");
+scanf("%s", nomecidade1); //por 'nomecidade1' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite a populacao da carta 1: \n");
+scanf("%d", &populacao1);
+
+printf("digite em km2 a area da cidade 1: \n");
+scanf("%f", &areacidade1);
+
+printf("digite o valor do PIB da cidade 1: \n");
+scanf("%f", &pibcidade1);
+
+printf("digite a quantidade de pontos turisticos da cidade 1: \n");
+scanf("%d", &pontosturisticos1);
+
+//saída de dados da carta 1
+
+printf("estado: %c\n", estado1);
+printf("codigo da carta: %s\n", codigocarta1);
+printf("nome da cidade: %s\n", nomecidade1);
+printf("populacao: %d\n", populacao1);
+printf("area: %f\n", areacidade1);
+printf("pib: %f\n", pibcidade1);
+printf("numero de pontos turisticos: %d\n", pontosturisticos1);
+
+/* iniciando o cadastro da carta 2, irei destacar neste comentário os nomes e valores correspondentes à mesma:
+estado: A
+codigo: A02
+nome da cidade: Vila velha 
+pupulação: 467.722 
+area: 210 km2
+pib: 16.305.842 BI 
+numero de pontos turisticos: 22 */
+
+// variaveis das carta 2:
+char estado2; // estado especificado apenas por uma letra, não é necessario array
+char codigocarta2[20]; 
+char nomecidade2[20];
+int populacao2;
+float areacidade2;
+float pibcidade2;
+int pontosturisticos2;
+
+// entrada de dados da carta 2:
+
+printf("digite o indicador do estado da carta 2 ('A' a 'H'): \n");
+scanf(" %c", &estado2); // espaço utilizado antes de '%c' para evitar que sejam deixados espaços em branco no buffer de entrada.
+
+printf("digite o codigo da carta 2: \n");
+scanf("%s", codigocarta2); // por 'codigocarta2' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite o nome da cidade 2: \n");
+scanf("%s", nomecidade2); //por 'nomecidade2' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite a populacao da carta 2: \n");
+scanf("%d", &populacao2);
+
+printf("digite em km2 a area da cidade 2: \n");
+scanf("%f", &areacidade2);
+
+printf("digite o valor do PIB da cidade 2: \n");
+scanf("%f", &pibcidade2);
+
+printf("digite a quantidade de pontos turisticos da cidade 2: \n");
+scanf("%d", &pontosturisticos2);
+
+//saída de dados da carta 2
+
+printf("estado: %c\n", estado2);
+printf("codigo da carta: %s\n", codigocarta2);
+printf("nome da cidade: %s\n", nomecidade2);
+printf("populacao: %d\n", populacao2);
+printf("area: %f\n", areacidade2);
+printf("pib: %f\n", pibcidade2);
+printf("numero de pontos turisticos: %d\n", pontosturisticos2);
+
+/* iniciando o cadastro da carta 3, irei destacar neste comentário os nomes e valores correspondentes à mesma:
+estado: A
+codigo: A03
+nome da cidade: Serra 
+pupulação: 572.274 
+area: 547.6 km2
+pib: 37.7 BI 
+numero de pontos turisticos: 23 */
+
+// variaveis das carta 3:
+char estado3; // estado especificado apenas por uma letra, não é necessario array
+char codigocarta3[20]; 
+char nomecidade3[20];
+int populacao3;
+float areacidade3;
+float pibcidade3;
+int pontosturisticos3;
+
+// entrada de dados da carta 3:
+
+printf("digite o indicador do estado da carta 3 ('A' a 'H'): \n");
+scanf(" %c", &estado3); // espaço utilizado antes de '%c' para evitar que sejam deixados espaços em branco no buffer de entrada.
+
+printf("digite o codigo da carta 3: \n");
+scanf("%s", codigocarta3); // por 'codigocarta3' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite o nome da cidade 3: \n");
+scanf("%s", nomecidade3); //por 'nomecidade3' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite a populacao da carta 3: \n");
+scanf("%d", &populacao3);
+
+printf("digite em km2 a area da cidade 3: \n");
+scanf("%f", &areacidade3);
+
+printf("digite o valor do PIB da cidade 3: \n");
+scanf("%f", &pibcidade3);
+
+printf("digite a quantidade de pontos turisticos da cidade 3: \n");
+scanf("%d", &pontosturisticos3);
+
+//saída de dados da carta 3
+
+printf("estado: %c\n", estado3);
+printf("codigo da carta: %s\n", codigocarta3);
+printf("nome da cidade: %s\n", nomecidade3);
+printf("populacao: %d\n", populacao3);
+printf("area: %f\n", areacidade3);
+printf("pib: %f\n", pibcidade3);
+printf("numero de pontos turisticos: %d\n", pontosturisticos3);
+
+/* iniciando o cadastro da carta 4, irei destacar neste comentário os nomes e valores correspondentes à mesma:
+estado: A
+codigo: A04
+nome da cidade: Cariacica
+pupulação: 353.491 
+area: 279.9 km2
+pib: 13.7 BI 
+numero de pontos turisticos: 16 */
+
+// variaveis das carta 4:
+char estado4; // estado especificado apenas por uma letra, não é necessario array
+char codigocarta4[20]; 
+char nomecidade4[20];
+int populacao4;
+float areacidade4;
+float pibcidade4;
+int pontosturisticos4;
+
+// entrada de dados da carta 4:
+
+printf("digite o indicador do estado da carta 4 ('A' a 'H'): \n");
+scanf(" %c", &estado4); // espaço utilizado antes de '%c' para evitar que sejam deixados espaços em branco no buffer de entrada.
+
+printf("digite o codigo da carta 4: \n");
+scanf("%s", codigocarta4); // por 'codigocarta4' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite o nome da cidade 4: \n");
+scanf("%s", nomecidade4); //por 'nomecidade4' ser um array, não será necesarria a utilização do operador '&'.
+
+printf("digite a populacao da carta 4: \n");
+scanf("%d", &populacao4);
+
+printf("digite em km2 a area da cidade 4: \n");
+scanf("%f", &areacidade4);
+
+printf("digite o valor do PIB da cidade 4: \n");
+scanf("%f", &pibcidade4);
+
+printf("digite a quantidade de pontos turisticos da cidade 4: \n");
+scanf("%d", &pontosturisticos4);
+
+//saída de dados da carta 4
+
+printf("estado: %c\n", estado4);
+printf("codigo da carta: %s\n", codigocarta4);
+printf("nome da cidade: %s\n", nomecidade4);
+printf("populacao: %d\n", populacao4);
+printf("area: %f\n", areacidade4);
+printf("pib: %f\n", pibcidade4);
+printf("numero de pontos turisticos: %d\n", pontosturisticos4);
+
 
     return 0;
 }
